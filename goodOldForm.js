ccca06d1eef67068fc0eab777a0b8f7320e1afac
Adam Deeley <ad.deeley@gmail.com>
2018-02-04 11:18:53 +0000

    Added a solution for the old form. Updated the solution to use pug.

---
+++ goodOldForm.js
@@ -0,0 +1,28 @@
+'use strict';
+const bodyparser = require('body-parser');
+const path = require('path');
+const port = process.argv[2];
+const express = require('express');
+const app = express();
+const publicDirectory = path.join(__dirname, 'public');
+
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static(publicDirectory))
+
+function reverseStr(str) {
+    return str.split('').reverse().join('');
+}
+
+app.get('/home', (req, res) => {
+    res.render('oldform')
+})
+app.post('/form', (req, res) => {
+    res.end(reverseStr(req.body.str));
+})
+
+app.listen(port, () => {
+    console.log('Listening on port: %o', port)
+})
---
+++ pug.js
@@ -0,0 +1,16 @@
+'use strict';
+const path = require('path');
+const port = process.argv[2];
+const express = require('express');
+const app = express()
+
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
+app.get('/home', (req, res) => {
+    res.render('index', { date: new Date().toDateString() })
+})
+
+app.listen(port, () => {
+    console.log('Listening on port: %o', port)
+})
