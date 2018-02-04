3252f3e30efc7a1080f89f024e7ef546180ca19e
Adam Deeley <ad.deeley@gmail.com>
2018-02-04 12:12:31 +0000

    Updated static to use Stylus css preprocessor.

--- static.js
+++ static.js
@@ -1,10 +1,12 @@
 'use strict';
 const path = require('path');
+const stylus = require('stylus').middleware('public');
 const express = require('express');
 const app = express();
 const port = process.argv[2];
 const dir = process.argv[3] || path.join(__dirname, 'public');
 
+app.use(stylus)
 app.use(express.static(dir));
 
 app.listen(port, () => {
